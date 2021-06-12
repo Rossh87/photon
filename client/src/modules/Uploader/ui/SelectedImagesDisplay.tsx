c73ef71670b547d7810b6449e80d5692edd86342
Rossh87 <hunter.dev.87@gmail.com>
2021-06-11 23:24:04 -0500

    continue styling Upload component

--- client/src/modules/Uploader/ui/SelectedImagesDisplay.tsx
+++ client/src/modules/Uploader/ui/SelectedImagesDisplay.tsx
@@ -1,8 +1,18 @@
 import React, { Dispatch } from 'react';
-import { TPreprocessingResults, IImage } from '../domain/domainTypes';
+import { TPreprocessingResults } from '../domain/domainTypes';
 import { IImageUploadState, TUploaderActions } from '../state/uploadStateTypes';
 import List from '@material-ui/core/List';
 import SelectedImage from './SelectedImage';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+	selectedFileContainer: {
+		border: '2px solid rgba(184,184,184, 0.5)',
+		borderRadius: '10px',
+		height: '50vh',
+		margin: theme.spacing(2),
+	},
+}));
 
 interface IDisplayProps {
 	uploadState: IImageUploadState;
@@ -14,6 +24,8 @@ const SelectedImagesDisplay: React.FunctionComponent<IDisplayProps> = ({
 	uploadState,
 	uploadDispatch,
 }) => {
+	const classes = useStyles();
+
 	const { selectedFiles } = uploadState;
 
 	const generateSelectedImageItems = (imageFiles: TPreprocessingResults) =>
@@ -25,11 +37,15 @@ const SelectedImagesDisplay: React.FunctionComponent<IDisplayProps> = ({
 			/>
 		));
 
-	return selectedFiles.length ? (
-		<List>
-			{generateSelectedImageItems(selectedFiles as TPreprocessingResults)}
-		</List>
-	) : null;
+	return (
+		<div className={classes.selectedFileContainer}>
+			<List>
+				{generateSelectedImageItems(
+					selectedFiles as TPreprocessingResults
+				)}
+			</List>
+		</div>
+	);
 };
 
 export default SelectedImagesDisplay;
