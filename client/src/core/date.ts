7c5e8428b2deb099aeb5e3a9532757e2b725e00f
Rossh87 <hunter.dev.87@gmail.com>
2021-09-08 01:54:57 -0500

    write formatting function for displaying dates

---
+++ client/src/core/date.ts
@@ -0,0 +1,34 @@
+// User info from database will include a 'joined on'
+// property that is output of Date.toJSON.  Here we format
+// it for display before storing it in app state.
+
+import { pipe } from 'fp-ts/lib/function';
+
+const mos = [
+	'January',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'October',
+	'November',
+	'December',
+];
+
+export const formatJoinDate = (dateString: string) => {
+	// dateString comes from Mongo ObjectId, so it's
+	// reasonably safe to assume it will be valid...right?
+	console.log('incoming string: ', dateString);
+	const d = new Date(dateString);
+
+	const month = mos[d.getMonth()];
+	console.log('month: ', month);
+	const date = d.getDate();
+	const year = d.getFullYear();
+
+	return `${month} ${date}, ${year}`;
+};
