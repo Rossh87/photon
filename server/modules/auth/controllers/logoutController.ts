a06afe60fd05b170486db1aa0f5ed153b7190a81
Rossh87 <hunter.dev.87@gmail.com>
2021-09-19 00:26:50 -0500

    allow session destruction callback to accept null

--- server/modules/auth/controllers/logoutController.ts
+++ server/modules/auth/controllers/logoutController.ts
@@ -1,8 +1,10 @@
 import { RequestHandler } from 'express';
+import { pipe } from 'fp-ts/lib/function';
+import { fromNullable, map } from 'fp-ts/lib/Option';
 import { LogoutFailureError } from '../domain/LogoutFailureError';
 
 export const logoutController: RequestHandler = (req, res, next) => {
 	req.session.destroy((e) =>
-		e !== undefined ? next(LogoutFailureError.create(e)) : res.end()
+		e ? next(LogoutFailureError.create(e)) : res.end()
 	);
 };
