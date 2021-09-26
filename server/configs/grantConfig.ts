8a6856dbbf15630d89de29cd9ca2cd8fa36f0967
Rossh87 <hunter.dev.87@gmail.com>
2021-09-25 20:51:13 -0500

    [server]: log specific err message for missing env vars
    print the name of missing variables at start time if environment
    secrets required by Grant OAuth middleware provider are missing

--- server/configs/grantConfig.ts
+++ server/configs/grantConfig.ts
@@ -14,14 +14,39 @@ const credentials = [
 	apiRoot,
 ];
 
+const credentialNameMap = [
+	'GOOGLE_OAUTH_CLIENT_APP_ID',
+	'GOOGLE_OAUTH_CLIENT_SECRET',
+	'GOOGLE_OAUTH_CLIENT_APP_ID',
+	'GITHUB_OAUTH_CLIENT_SECRET',
+	'API_ROOT',
+];
+
 // completely implode if these are missing--app won't work at all
-credentials.forEach((cred) => {
-	if (cred === undefined) {
-		throw new Error(
-			'Grant config: missing needed credentials. Please check env variables'
-		);
+const checkEnv = () => {
+	const missing: number[] = [];
+
+	credentials.forEach((cred, idx) => {
+		if (typeof cred !== 'string') {
+			missing.push(idx);
+		}
+	});
+
+	if (missing.length > 0) {
+		const errString = missing.reduce((s, missingNumber) => {
+			s += `Missing from env: ${credentialNameMap[missingNumber]}\n`;
+			return s;
+		}, '');
+
+		console.error(errString);
+
+		throw new Error('Grant config: missing required environment variables');
 	}
-});
+
+	return;
+};
+
+checkEnv();
 
 const grantConfig: GrantConfig = {
 	defaults: {
