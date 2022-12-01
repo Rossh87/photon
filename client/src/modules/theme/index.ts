abde1e76dcac1afba6d27d9a13746a3b90e12db8
Rossh87 <hunter.dev.87@gmail.com>
2022-11-30 23:05:27 -0600

    [client]: convert theme to MUI v5 and remove shim function

--- client/src/modules/theme/index.ts
+++ client/src/modules/theme/index.ts
@@ -1,112 +1,18 @@
-import { adaptV4Theme, createTheme } from '@mui/material/styles';
-import deepPurple from '@mui/material/colors/deepPurple';
+import { createTheme } from '@mui/material/styles';
 
-let theme = createTheme(
-	adaptV4Theme({
-		// palette: {
-		//     primary: {
-		//         light: '#63ccff',
-		//         main: '#009be5',
-		//         dark: '#006db3',
-		//     },
-		// },
-		palette: {
-			primary: {
-				main: deepPurple[700],
-			},
+let theme = createTheme({
+	palette: {
+		primary: {
+			light: '#9162e4',
+			main: '#5e35b1',
+			dark: '#280680',
 		},
-		typography: {
-			h5: {
-				fontWeight: 500,
-				fontSize: 26,
-				letterSpacing: 0.5,
-			},
+		secondary: {
+			main: '#c2185b',
+			light: '#fa5788',
+			dark: '#8c0032',
 		},
-		shape: {
-			borderRadius: 8,
-		},
-		props: {
-			MuiTab: {
-				disableRipple: false,
-			},
-		},
-		mixins: {
-			toolbar: {
-				minHeight: 48,
-			},
-		},
-		// overrides: {
-		// 	MuiButton: {
-		// 		label: {
-		// 			textTransform: 'none',
-		// 		},
-		// 		contained: {
-		// 			boxShadow: 'none',
-		// 			'&:active': {
-		// 				boxShadow: 'none',
-		// 			},
-		// 		},
-		// 	},
-		// 	MuiTabs: {
-		// 		root: {
-		// 			marginLeft: theme.spacing(1),
-		// 		},
-		// 		indicator: {
-		// 			height: 3,
-		// 			borderTopLeftRadius: 3,
-		// 			borderTopRightRadius: 3,
-		// 			backgroundColor: theme.palette.common.white,
-		// 		},
-		// 	},
-		// 	MuiTab: {
-		// 		root: {
-		// 			textTransform: 'none',
-		// 			margin: '0 16px',
-		// 			minWidth: 0,
-		// 			padding: 0,
-		// 			[theme.breakpoints.up('md')]: {
-		// 				padding: 0,
-		// 				minWidth: 0,
-		// 			},
-		// 		},
-		// 	},
-		// 	MuiIconButton: {
-		// 		root: {
-		// 			padding: theme.spacing(1),
-		// 		},
-		// 	},
-		// 	MuiTooltip: {
-		// 		tooltip: {
-		// 			borderRadius: 4,
-		// 		},
-		// 	},
-		// 	MuiDivider: {
-		// 		root: {
-		// 			backgroundColor: '#404854',
-		// 		},
-		// 	},
-		// 	MuiListItemText: {
-		// 		primary: {
-		// 			fontWeight: theme.typography.fontWeightMedium,
-		// 		},
-		// 	},
-		// 	MuiListItemIcon: {
-		// 		root: {
-		// 			color: 'inherit',
-		// 			marginRight: 0,
-		// 			'& svg': {
-		// 				fontSize: 20,
-		// 			},
-		// 		},
-		// 	},
-		// 	MuiAvatar: {
-		// 		root: {
-		// 			width: 32,
-		// 			height: 32,
-		// 		},
-		// 	},
-		// },
-	})
-);
+	},
+});
 
 export default theme;
